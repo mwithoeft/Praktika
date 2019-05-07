ed95ffb56bcb079d0a2b83a76a6986dda4397ff4
Moritz Withöft <moritz_pit.withoeftQfh-bielefeld.de>
2019-05-07 12:58:45 +0200

    JavaScript added

---
+++ Web/Praktikum 05a/docroot/js/lang.js
@@ -0,0 +1,25 @@
+let de = {
+    start: "Startseite",
+    list: "Liste der Projekte",
+    create: "Projekt erstellen",
+    short_description: "Kurzbeschreibung",
+    long_description: "Langbeschreibung",
+    to_project: "Zur Projektseite!",
+    rating: "Bewertung",
+    project_goals: "Projektziele",
+    project_information: "Projektinformation",
+    additional: "Zusätzliches"
+};
+
+let en = {
+    start: "Home",
+    list: "List of projects",
+    create: "Create a project",
+    short_description: "Short description",
+    long_description: "Long description",
+    to_project: "To project!",
+    rating: "Rating",
+    project_goals: "Project goals",
+    project_information: "Project information",
+    additional: "Additional"
+};
