2720d09b2cfd06321ce642dcd5ae0623f2636315
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-05-10 13:53:18 +0200

    Aufgabe 4

--- Embedded/04/interrupt/interrupt.ino
+++ Embedded/04/interrupt/interrupt.ino
@@ -8,23 +8,10 @@ const uint8_t ButtonPinIn = PC_4;
 // define delay for blinking in ms
 const uint32_t Delay = 2000;
 
-//! LED handling class. Has disable() function for emergency stop.
-//! Parameter (in): PORT_NB (output port for connected led)
-template <const uint8_t PORT_NB>
+uint8_t m_ledState = LOW; // current state of led
+bool m_disabled = false; // disable flag (on if led is finally turned off)
 
-
-class TLed {
-  public:
-    //! Constructor takes state (HIGH, LOW) only if given.
-    //! Defaults: value for state = LOW, and is not disabled.
-    TLed(const uint8_t f_ledState = LOW)
-      : m_ledState(f_ledState), m_disabled(false){
-      pinMode(PORT_NB, OUTPUT); // led is always output
-      digitalWrite(PORT_NB, m_ledState); // set led to default state
-    }
-    //! If this led is disable, nothing happens, otherwise
-    //! toggles state of led (from HIGH to LOW or from LOW to HIGH).
-    void toggle() {
+void toggle() {
       if (m_disabled) // somehow no longer active
         return;
       if (m_ledState == LOW) { // toggle state
@@ -33,36 +20,22 @@ class TLed {
       else {
         m_ledState = LOW;
       }
-      digitalWrite(PORT_NB, m_ledState); // set led to current state
-    }
-    //! Turn led finally off (emergency stop), state is set LOW, functionality off.
-    void off() {
-      m_disabled = true;
-      m_ledState = LOW;
-      digitalWrite(PORT_NB, m_ledState); // set led to current state
-    }
-    
-  private:
-    uint8_t m_ledState; // current state of led
-    bool m_disabled; // disable flag (on if led is finally turned off)
-};
-
-// global instances for led output
-TLed<LedPortOut> Led;
-
+      digitalWrite(LedPortOut, m_ledState); // set led to current state
+}
+void off() {
+  m_disabled = true;
+  m_ledState = LOW;
+  digitalWrite(LedPortOut, m_ledState); // set led to current state
+}
 
 void setup() {
-  void (*func)(void) =  TLed<LedPortOut>::off
-  IntRegister(ButtonPinIn, fp);
+  pinMode(LedPortOut, OUTPUT); // led is always output
+  IntRegister(ButtonPinIn, &off);
   IntEnable(ButtonPinIn);
   IntMasterEnable();
-  
 }
 void loop() {
-
-  IntMasterEnable();
-  Led.toggle();
-
+  toggle();
   // wait
   delay(Delay);
 }
