5ed53fb34bbc010c90914c95ec0e385fb3b6477c
Moritz Withöft <moritz_pit.withoeftQfh-bielefeld.de>
2019-05-13 14:34:20 +0200

    Betriebssysteme Aufgabe 2 fertig -> Hannes Du bist dran

--- Betriebssysteme/03/delete.sh
+++ Betriebssysteme/03/delete.sh
@@ -24,6 +24,7 @@ function deleteFile {
 	if [ -f $FILE ]; then
 		mv $FILE $HOME/.trashBin/"$TIMESTAMP"_"$ID"".dat"
 		echo "$TIMESTAMP"_"$ID"".dat!" "$ABSPATH" &>> $HOME/.trashBin/.dir
+		echo $FILE "has been moved to TrashBin and was renamed to $TIMESTAMP"_"$ID"".dat"
 	else
 		echo $FILE "was not Found!"
 	fi
