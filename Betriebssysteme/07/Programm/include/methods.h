b0df1067e959e0e2d519df058486185b5bdcef76
Hannes Rüffer <hannes.rueffer@fh-bielefeld.de>
2019-06-22 15:35:11 +0000

    BS07 Aufgabe 3 fertig

--- Betriebssysteme/07/Programm/include/methods.h
+++ Betriebssysteme/07/Programm/include/methods.h
@@ -7,9 +7,13 @@
 #define IOOUT 1
 #define IOERR 2
 
+#define SQUEEZE_LEAD 11
+#define SQUEEZE_END 10
+
 
 void printSecondHalfPrintFirstHalf (const int);
 off_t getFileSize(const int);
 int writeFromBuffer(const int, const char*, size_t);
-
+void writeFile(const int input, const int output);
+void squeezeFile(const int input, const int output);
 #endif //METHODS_H
--- Betriebssysteme/07/Programm/main.c
+++ Betriebssysteme/07/Programm/main.c
@@ -28,24 +28,35 @@ int main(int argc, char *argv[])
     /* Öffnen der Eingabe-Datei */
     char *src = argv[1];
     int filedescriptor_in = open(src, O_RDONLY);
-    if (filedescriptor_in < -1)
+    if (filedescriptor_in < 0)
     {
         char *error = "Datei konnte nicht geöffnet werden\n";
         write(IOERR, error, strlen(error));
         return EXIT_FAILURE;
     }
+
+    char *message = "Zweite Haelfte / Erste Haelfte:\n";
+    write(IOOUT, message, strlen(message));
 
     printSecondHalfPrintFirstHalf(filedescriptor_in);
 
     /* Öffnen der Ausgabe-Datei */
     char *dest = argv[2];
     int filedescriptor_out = open(dest, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (filedescriptor_out < -1)
+    if (filedescriptor_out < 0)
     {
         char *error = "Datei konnte nicht geöffnet werden\n";
         write(IOERR, error, strlen(error));
         return EXIT_FAILURE;
     }
+
+    //writeFile(filedescriptor_in, filedescriptor_out);
+    
+
+    message = "Inhalt der Ausgabedatei nach dem Kopieren:\n";
+    write(IOOUT, message, strlen(message));
+
+    squeezeFile(filedescriptor_in, filedescriptor_out);
 
     /* Schließen der Eingabe-Datei */
     if (close(filedescriptor_in))
@@ -56,6 +67,26 @@ int main(int argc, char *argv[])
     }
     /*  Schließen der Ausgabe-Datei */
     if (close(filedescriptor_out))
+    {
+        char *error = "Ausgabe-Datei konnte nicht geschlossen werden\n";
+        write(IOERR, error, strlen(error));
+        return EXIT_FAILURE;
+    }
+
+    /* Öffnen der Ausgabe-Datei */
+    char *srcP = argv[2];
+    filedescriptor_in = open(srcP, O_RDONLY);
+    if (filedescriptor_in < 0)
+    {
+        char *error = "Datei konnte nicht geöffnet werden\n";
+        write(IOERR, error, strlen(error));
+        return EXIT_FAILURE;
+    }
+
+    writeFile(filedescriptor_in, IOOUT);
+
+    /*  Schließen der Ausgabe-Datei */
+    if (close(filedescriptor_in))
     {
         char *error = "Ausgabe-Datei konnte nicht geschlossen werden\n";
         write(IOERR, error, strlen(error));
